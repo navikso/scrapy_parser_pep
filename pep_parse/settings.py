7eb133325ea8ce016f5c0b6da07ff2b2b4c7c27e
Anastasia Sokolova <navukso@ya.ru>
2023-09-30 18:03:37 +0300

    final

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -1,23 +1,21 @@
 from pathlib import Path
 
 
-BOT_NAME = "pep_parse"
+BOT_NAME = 'pep_parse'
 BASE_DIR = Path(__file__).parent.parent
 
 RESULTS_PATH = 'results'
 
-SPIDER_MODULES = ["pep_parse.spiders"]
-NEWSPIDER_MODULE = "pep_parse.spiders"
+SPIDER_MODULES = ['pep_parse.spiders']
+NEWSPIDER_MODULE = 'pep_parse.spiders'
 
 ROBOTSTXT_OBEY = True
 
-ITEM_PIPELINES = {
-   "pep_parse.pipelines.PepParsePipeline": 300,
-}
+ITEM_PIPELINES = {'pep_parse.pipelines.PepParsePipeline': 300, }
 
-REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
-TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
-FEED_EXPORT_ENCODING = "utf-8"
+REQUEST_FINGERPRINTER_IMPLEMENTATION = '2.7'
+TWISTED_REACTOR = 'twisted.internet.asyncioreactor.AsyncioSelectorReactor'
+FEED_EXPORT_ENCODING = 'utf-8'
 
 FEEDS = {
     f'{RESULTS_PATH}/pep_%(time)s.csv': {
