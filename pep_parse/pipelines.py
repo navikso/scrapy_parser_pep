e5c028ce6980983293b9b602d2ed332ddc865067
Anastasia Sokolova <navukso@ya.ru>
2023-10-02 11:17:45 +0300

    review_1

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,18 +1,25 @@
 from collections import defaultdict
 from datetime import datetime
 from .settings import BASE_DIR
+from scrapy.exceptions import DropItem
+
+
+TABLE_COLUMNS = "Статус,Количество\n"
 
 
 class PepParsePipeline:
-
     def __init__(self):
         self.status_counter = defaultdict(int)
 
     def open_spider(self, spider):
+        # Если этот метод удалить, Pytest выдает:
+        # "В классе `PepParsePipeline` должен быть метод `open_spider`."
         pass
 
     def process_item(self, item, spider):
-        self.status_counter[item['status']] += 1
+        if not item["status"] in self.status_counter:
+            raise DropItem("'Status' is not found")
+        self.status_counter[item["status"]] += 1
         return item
 
     def close_spider(self, spider):
@@ -20,10 +27,11 @@ class PepParsePipeline:
         datetime.now()
 
         with open(
-                f'{BASE_DIR}/results/status_summary_{datetime.now()}.csv',
-                mode='w',
-                encoding='utf-8') as f:
-            f.write('Статус,Количество\n')
+            f"{BASE_DIR}/results/status_summary_{datetime.now()}.csv",
+            mode="w",
+            encoding="utf-8",
+        ) as f:
+            f.write(TABLE_COLUMNS)
             for status, count in self.status_counter.items():
-                print(f'{status}, {count}', file=f)
-            f.write(f'Total,{total}\n')
+                print(f"{status}, {count}", file=f)
+            f.write(f"Total,{total}\n")
